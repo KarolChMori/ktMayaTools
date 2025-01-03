a603b305fd6bc27157378d2839074c1173ec3749
KarolChale <karolchale@hotmail.com>
2025-01-03 02:17:20 +0100

    Testing RangeSlider but to be float

--- kt_randomizer.py
+++ kt_randomizer.py
@@ -23,14 +23,20 @@ class TestDialog(QtWidgets.QDialog):
 
     def createWidgets(self):
         self.selBTN = QtWidgets.QPushButton("New Sel")
-        self.selNumLBL = QtWidgets.QLabel('TEST')
-        self.selSLD = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.selSLD = ktW.ktRangeSlider(minValue=0, maxValue=1, showMinMaxField=False, stepSize=0.1)
         self.resultBTN = QtWidgets.QPushButton("New Result")
         self.retouchBTN = QtWidgets.QPushButton("Retouch")
         self.clearBTN = QtWidgets.QPushButton("Clear")
 
         # --------------------------------------------
-        self.translateSLD = ktW.ktRangeSlider()
+        self.optionsCMB = QtWidgets.QComboBox()
+        self.optionsCMB.addItem('Translate')
+        self.optionsCMB.addItem('Rotate')
+        self.optionsCMB.addItem('Scale')
+        self.xCB = QtWidgets.QCheckBox()
+        self.yCB = QtWidgets.QCheckBox()
+        self.zCB = QtWidgets.QCheckBox()
+        self.translateSLD = ktW.ktRangeSlider(textWidth=55)
 
 
     def createLayouts(self):
@@ -39,21 +45,27 @@ class TestDialog(QtWidgets.QDialog):
         
         mainGridLYT = QtWidgets.QGridLayout(self)
         mainGridLYT.addWidget(self.selBTN, 0,0)
-        mainGridLYT.addWidget(self.selNumLBL, 0,1)
-        mainGridLYT.addWidget(self.selSLD, 0,2)
-        mainGridLYT.addWidget(self.resultBTN, 0,3)
-        mainGridLYT.addWidget(self.retouchBTN, 0,4)
-        mainGridLYT.addWidget(self.clearBTN, 0,5)
+        mainGridLYT.addWidget(self.selSLD, 0,1)
+        mainGridLYT.addWidget(self.resultBTN, 0,2)
+        mainGridLYT.addWidget(self.retouchBTN, 0,3)
+        mainGridLYT.addWidget(self.clearBTN, 0,4)
 
         coordGridLYT = QtWidgets.QGridLayout(self)
+        #coordGridLYT.setAlignment(QtCore.Qt.AlignCenter)
         coordGridLYT.addWidget(QtWidgets.QLabel('X'), 1,1)
         coordGridLYT.addWidget(QtWidgets.QLabel('Y'), 1,2)
         coordGridLYT.addWidget(QtWidgets.QLabel('Z'), 1,3)
-        coordGridLYT.addWidget(QtWidgets.QLabel('Multiply'), 1,4)
-        coordGridLYT.addWidget(QtWidgets.QLabel('Range'), 1,5)
-        coordGridLYT.addWidget(QtWidgets.QLabel('Min'), 1,6)
-        coordGridLYT.addWidget(QtWidgets.QLabel('Max'), 1,7)
-        coordGridLYT.addWidget(self.translateSLD, 2,5,1,3)
+        coordGridLYT.addWidget(QtWidgets.QLabel('Range'), 1,4)
+        coordGridLYT.addWidget(QtWidgets.QLabel('Min'), 1,5)
+        coordGridLYT.addWidget(QtWidgets.QLabel('Max'), 1,6)
+        coordGridLYT.addWidget(self.optionsCMB, 2,0)
+        coordGridLYT.addWidget(self.xCB, 2,1)
+        coordGridLYT.addWidget(self.yCB, 2,2)
+        coordGridLYT.addWidget(self.zCB, 2,3)
+        coordGridLYT.addWidget(self.translateSLD, 2,4,1,2)
+
+        
+
 
         mainLayout.addLayout(mainGridLYT)
         mainLayout.addLayout(coordGridLYT)
--- util/kt_widgets.py
+++ util/kt_widgets.py
@@ -2,52 +2,106 @@ from PySide2 import QtCore
 from PySide2 import QtWidgets
 
 class ktRangeSlider(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, textWidth=60, sliderWidth=150, minValue=0, maxValue=10, showValueField=True, showMinMaxField=True, stepSize=1):
         super().__init__()
+
+        self.textWidth = textWidth
+        self.sliderWidth = sliderWidth
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.showValueField = showValueField
+        self.showMinMaxField = showMinMaxField
+        self.stepSize = stepSize
+        
 
         self.createWidgets()
         self.createLayouts()
         self.createConnections()
 
     def createWidgets(self):
+        # Scaling factor to allow fractional precision
+        self.scaleFactor = 100 
+        
+        # Scale values to integers
+        self.minValueScaled = int(self.minValue * self.scaleFactor)
+        self.maxValueScaled = int(self.maxValue * self.scaleFactor)
+        self.stepSizeScaled = int(self.stepSize * self.scaleFactor)
+
+        
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.slider.setMinimum(0)  # Default minimum value
-        self.slider.setMaximum(100)  # Default maximum value
-        self.minInputTXT = QtWidgets.QLineEdit()
-        self.maxInputTXT = QtWidgets.QLineEdit()
+        self.slider.setFixedWidth(self.sliderWidth)
+        self.slider.setMinimum(self.minValueScaled)
+        self.slider.setMaximum(self.maxValueScaled)
+        self.slider.setTickInterval(self.stepSizeScaled)
+        self.slider.setSingleStep(self.stepSizeScaled)
+
+        # Create QDoubleSpinBox for min and max
+        self.minInputTXT = QtWidgets.QDoubleSpinBox()
+        self.minInputTXT.setFixedWidth(self.textWidth)
+        self.minInputTXT.setValue(self.minValue)
+        self.minInputTXT.setSingleStep(self.stepSize)
+
+        self.maxInputTXT = QtWidgets.QDoubleSpinBox()
+        self.maxInputTXT.setFixedWidth(self.textWidth)
+        self.maxInputTXT.setValue(self.maxValue)
+        self.maxInputTXT.setSingleStep(self.stepSize)
+
+        # Create QDoubleSpinBox for the slider's value
+        self.valueField = QtWidgets.QDoubleSpinBox()
+        self.valueField.setRange(self.minValue, self.maxValue)
+        self.valueField.setFixedWidth(self.textWidth)
+        self.valueField.setSingleStep(self.stepSize)
+        self.valueField.setValue(self.slider.value())
+        
 
     def createLayouts(self):
         mainLayout = QtWidgets.QHBoxLayout(self)
+
+        if self.showValueField:
+            mainLayout.addWidget(self.valueField)
+        
         mainLayout.addWidget(self.slider)
-        mainLayout.addWidget(self.minInputTXT)
-        mainLayout.addWidget(self.maxInputTXT)
+
+        if self.showMinMaxField:
+            mainLayout.addWidget(self.minInputTXT)
+            mainLayout.addWidget(self.maxInputTXT)
 
     def createConnections(self):
         # Connect input fields to update slider range
-        self.minInputTXT.editingFinished.connect(self.updateMin)
-        self.maxInputTXT.editingFinished.connect(self.updateMax)
+        self.minInputTXT.valueChanged.connect(self.updateMin)
+        self.maxInputTXT.valueChanged.connect(self.updateMax)
+
+        # Connect the slider's value change to update the updateValueField
+        self.slider.valueChanged.connect(self.updateValueField)
+
+        # Connect the valueField to update the slider
+        self.valueField.valueChanged.connect(self.updateSliderValue)
 
     def updateMin(self):
         """Update the slider's minimum value based on the input."""
-        try:
-            minValue = int(self.minInputTXT.text())
-            if minValue < self.slider.maximum():
-                self.slider.setMinimum(minValue)
-            else:
-                self.minInputTXT.setText(str(self.slider.minimum()))  # Revert to valid value
-        except ValueError:
-            self.minInputTXT.setText(str(self.slider.minimum()))  # Revert to valid value
+        minValue = self.minInputTXT.value()
+        # Only update if the minValue is less than the maxValue
+        if minValue < self.maxInputTXT.value():
+            self.slider.setMinimum(minValue)
+        else:
+            self.minInputTXT.setValue(self.slider.minimum())  # Revert to valid value
 
     def updateMax(self):
         """Update the slider's maximum value based on the input."""
-        try:
-            maxValue = int(self.maxInputTXT.text())
-            if maxValue > self.slider.minimum():
-                self.slider.setMaximum(maxValue)
-            else:
-                self.maxInputTXT.setText(str(self.slider.maximum()))  # Revert to valid value
-        except ValueError:
-            self.maxInputTXT.setText(str(self.slider.maximum()))  # Revert to valid value
+        maxValue = self.maxInputTXT.value()
+        # Only update if the maxValue is greater than the minValue
+        if maxValue > self.minInputTXT.value():
+            self.slider.setMaximum(maxValue)
+        else:
+            self.maxInputTXT.setValue(self.slider.maximum())  # Revert to valid value
+    
+    def updateValueField(self):
+        """Update the value of the spinbox when the slider moves."""
+        self.valueField.setValue(self.slider.value())
+
+    def updateSliderValue(self):
+        """Update the slider's value when the spinbox value changes."""
+        self.slider.setValue(self.valueField.value() )
     
     def getValue(self):
         return self.slider.value()
